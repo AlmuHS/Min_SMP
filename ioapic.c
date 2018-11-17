cf6d4b3afc130853e78ea85ca8922d6da3b6159b
AlmuHS <liberamenso10000@gmail.com>
2018-11-17 01:25:53 +0100

    anadida cabecera GPL

--- ioapic.c
+++ ioapic.c
@@ -1,4 +1,16 @@
-
+/*Copyright 2018 Juan Bosco Garcia
+ *This file is part of Min_SMP. 
+ *Min_SMP is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *Min_SMP is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *You should have received a copy of the GNU General Public License
+ *along with Min_SMP.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <ioapic.h>
 
