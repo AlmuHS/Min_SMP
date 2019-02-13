c02eacc5e118c6b00704dac9fa4cc255f7ccf18f
AlmuHS <liberamenso10000@gmail.com>
2019-02-13 18:31:15 +0100

    added lapic table

--- include/lapic.h
+++ include/lapic.h
@@ -24,23 +24,131 @@
 */
 
 
-/*struct lapic_table{
-*	uint32 reserved1;
-*	uint16 apic_id;
-*	const uint16 apic_version;
-*	uint32 reserved2;
-*	uint32 reserved3;
-*	const uint16 tpr;
-*	const uint16 apr;
-*	const uint16 ppr;
-*	uint16 eoi;
-*	const uint16 rrd;
-*	uint16 ldr;
-*	uint16 int_vector;
-*	uint64 isr_1;
-*	uint64 isr_2;
-*};
-*/
+typedef struct ApicReg
+{
+	unsigned r;	/* the actual register */
+	unsigned p[3];	/* pad to the next 128-bit boundary */
+} ApicReg;
+
+typedef struct ApicIoUnit
+{
+	ApicReg select;
+	ApicReg window;
+} ApicIoUnit;
+
+typedef enum type_enum{
+	Fixed = 0b000,
+	LowestPriority = 0b001,
+	SMI = 0b010,
+	NMI = 0b011,
+	INIT = 0b101,
+	StartUp = 0b110
+} icr_type;
+
+typedef enum dest_mode_enum{
+	Physical = 0b0,
+	Logical = 0b1
+} icr_dest_mode;
+
+typedef enum deliv_status_enum{
+	Idle = 0b0,
+	SendPending = 0b1
+} icr_deliv_status;
+
+typedef enum level_enum{
+	De_assert = 0b0,
+	Assert = 0b1
+} icr_level; 
+
+typedef enum trig_mode_enum{
+	Edge = 0b0,
+	Level = 0b1
+} icr_trig_mode;
+
+typedef enum dest_sh_enum{
+	NoShortHand = 0b00,
+	Self = 0b01,
+	AllIncludingSelf = 0b10,
+	AllExcludingSelf = 0b11
+} icr_dest_sh;
+ 	
+
+typedef struct icr_low{
+	uint8 vector:8;
+	icr_type type:3;
+	icr_dest_mode dest_mode:1;
+	icr_deliv_status deliv_status:1;
+	uint8 reserved1:1;
+	icr_level level:1;
+	icr_trig_mode trigger_mode:1;
+	uint8 reserved2:2;
+	icr_dest_sh dest_shorthand:2;
+	uint16 reserved3:12;
+} icrl;
+
+typedef struct icr_high{
+	uint32 reserved:24;
+	uint8 dest:8;
+} icrh;
+
+typedef struct ApicICRHReg
+{
+	icrh r;	/* the actual register */
+	unsigned p[3];	/* pad to the next 128-bit boundary */
+} ApicICRHReg;
+
+typedef struct ApicICRLReg
+{
+	icrl r;	/* the actual register */
+	unsigned p[3];	/* pad to the next 128-bit boundary */
+} ApicICRLReg;
+
+
+#define APIC_IO_UNIT_ID			0x00
+#define APIC_IO_VERSION			0x01
+#define APIC_IO_REDIR_LOW(int_pin)	(0x10+(int_pin)*2)
+#define APIC_IO_REDIR_HIGH(int_pin)	(0x11+(int_pin)*2)
+
+typedef struct ApicLocalUnit
+{
+	ApicReg reserved0;
+	ApicReg reserved1;
+	ApicReg apic_id;
+	ApicReg version;
+	ApicReg reserved4;
+	ApicReg reserved5;
+	ApicReg reserved6;
+	ApicReg reserved7;
+	ApicReg task_pri;
+	ApicReg arbitration_pri;
+	ApicReg processor_pri;
+	ApicReg eoi;
+	ApicReg remote;
+	ApicReg logical_dest;
+	ApicReg dest_format;
+	ApicReg spurious_vector;
+	ApicReg isr[8];
+	ApicReg tmr[8];
+	ApicReg irr[8];
+	ApicReg error_status;
+	ApicReg reserved28[7];
+	ApicICRLReg icr_low;
+	ApicICRHReg icr_high;
+	ApicReg timer_vector;
+	ApicReg thermal_sensor;
+	ApicReg performance_monitor;
+	ApicReg lint0_vector;
+	ApicReg lint1_vector;
+	ApicReg error;
+	ApicReg init_count;
+	ApicReg cur_count;
+	ApicReg reserved3a;
+	ApicReg reserved3b;
+	ApicReg reserved3c;
+	ApicReg reserved3d;
+	ApicReg divider_config;
+	ApicReg reserved3f;
+} ApicLocalUnit;
 
 //static void lapicw(int index, int value);
 void lapicinit(void);
--- include/mp.h
+++ include/mp.h
@@ -16,6 +16,7 @@
 #define __MP_H__
 
 #include <types.h>
+#include <lapic.h>
 
 #define NCPU 128
 
@@ -25,71 +26,6 @@ struct cpu {
 };
 
 
-enum type_enum{
-	Fixed = 0b000,
-	LowestPriority = 0b001,
-	SMI = 0b010,
-	NMI = 0b011,
-	INIT = 0b101,
-	StartUp = 0b110
-};
-
-enum dest_mode_enum{
-	Physical = 0b0,
-	Logical = 0b1
-};
-
-enum deliv_status_enum{
-	Idle = 0b0,
-	SendPending = 0b1
-};
-
-enum level_enum{
-	De_assert = 0b0,
-	Assert = 0b1
-};
-
-enum trig_mode_enum{
-	Edge = 0b0,
-	Level = 0b1
-};
-
-enum dest_sh_enum{
-	NoShortHand = 0b00,
-	Self = 0b01,
-	AllIncludingSelf = 0b10,
-	AllExcludingSelf = 0b11
-};
- 	
-
-typedef enum type_enum type_t;
-typedef enum dest_mode_enum dest_mode_t;
-typedef enum deliv_status_enum deliv_status_t;
-typedef enum level_enum level_t;
-typedef enum trig_mode_enum trig_mode_t;
-typedef enum dest_sh_enum dest_sh_t;
-
-struct icr_low{
-	uint8 vector:8;
-	type_t type:3;
-	dest_mode_t dest_mode:1;
-	deliv_status_t deliv_status:1;
-	uint8 reserved1:1;
-	level_t level:1;
-	trig_mode_t trigger_mode:1;
-	uint8 reserved2:2;
-	dest_sh_t dest_shorthand:2;
-	uint16 reserved3:12;
-};
-
-struct icr_high{
-	uint32 reserved:24;
-	uint8 dest:8;
-};
-
-typedef struct icr_low icrl;
-typedef struct icr_high icrh;
-
 int mp_setup();
 
 void startup_cpu(uint8 apic_id);
@@ -98,14 +34,14 @@ void mp_print_info();
 
 int16 cpu_number();
 
-void write_icr_type(type_t type);
-void write_icr_destmode(dest_mode_t dm);
-void write_icr_level(level_t level);
-void write_icr_trigmode(trig_mode_t trigger_mode);
-void write_icr_destsh(dest_sh_t dest_sh);
+void write_icr_type(icr_type type);
+void write_icr_destmode(icr_dest_mode dm);
+void write_icr_level(icr_level level);
+void write_icr_trigmode(icr_trig_mode trigger_mode);
+void write_icr_destsh(icr_dest_sh dest_sh);
 void write_icr_dest(uint8 dest);
 
-void send_IPI(struct icr_high icr_h, struct icr_low icr_l);
+void send_IPI(icrh icr_h, icrl icr_l);
 
 
 
